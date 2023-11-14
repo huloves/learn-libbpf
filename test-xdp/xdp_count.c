ce6fb03080d0d5a82ed19cd3d771568796c43225
huloves <17791627852@163.com>
2023-11-14 18:36:26 +0800

    add notes

--- test-xdp/xdp_count.c
+++ test-xdp/xdp_count.c
@@ -1,3 +1,20 @@
+/**
+ * prepare:
+ * git clone git@github.com:xdp-project/xdp-tutorial.git
+ * cd xdp-tutorial/testenv
+ * sudo ./testenv.sh setup --name=test1 --legacy-ip
+ * 
+ * clang -O2 -Wall -g -target bpf -c xdp_count.c -o xdp_count.o
+ * 
+ * sudo bpftool prog load xdp_count.o /sys/fs/bpf/xdp_count type xdp
+ * sudo bpftool prog list
+ * sudo bpftool net attach xdpgeneric id <program_id> dev test1
+ * 
+ * sudo ./testenv.sh ping # For IPv6
+ * sudo ./testenv.sh ping --legacy-ip # For IPv4
+ * 
+ * sudo bpftool map dump id <map_id>
+ */
 // #include "vmlinux.h"
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
@@ -12,6 +29,13 @@ struct {
 	__type(key, __u32);
 	__type(value, long);
 } cnt SEC(".maps");
+
+// struct bpf_map_def SEC("maps") cnt = {
+//         .type = BPF_MAP_TYPE_ARRAY,
+//         .key_size = sizeof(__u32),
+//         .value_size = sizeof(long),
+//         .max_entries = 2,
+// };
 
 SEC("xdp_count")
 int xdp_count_prog(struct xdp_md *ctx)
